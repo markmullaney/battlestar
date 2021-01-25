bfacd0c6b64e35b48a14ac73ca3326cdf57e9c5b
Mark Mullaney <mmullaney@gmail.com>
2021-01-25 07:31:11 +0000

    fix account issue

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 require('newrelic')
 const express = require('express')
-const app = express()
-const port = 3000
+const ngrok = require('ngrok')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
